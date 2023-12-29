68073fe6a941513b91f221fd56d9b0a0db3eee4e
Alex Hamlin <alex@alexhamlin.co>
2023-12-29 07:43:19 -0800

    Write a skeleton for the real isopod daemon

--- isopod/__init__.py
+++ isopod/__init__.py
@@ -1,7 +1,22 @@
+import argparse
 import logging
+import os
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s %(name)s %(levelname)s: %(message)s",
     datefmt="%F %T",
 )
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Watch the CD-ROM drive and rip every disc to a remote server"
+    )
+    parser.parse_args()
+
+    if os.getuid() != 0:
+        log.critical("Not running as root")
+        return 1
