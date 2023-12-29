bdf1b9162e0fd51b419927cc9352ba0c69d097a3
Alex Hamlin <alex@alexhamlin.co>
2023-12-29 06:54:57 -0800

    Start implementing logging

--- isopod/__init__.py
+++ isopod/__init__.py
@@ -1,16 +1,27 @@
 import sys
+import logging
 
 from pyudev import Context, Monitor
 
 from isopod.cdrom import get_drive_status
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)-9s %(name)s: %(message)s",
+)
+log = logging.getLogger("isopod")
+
 
 def status():
     if len(sys.argv) < 2:
-        print("Need a device to check", file=sys.stderr)
+        log.fatal("Need a device to check")
         return 1
 
-    print(get_drive_status(sys.argv[1]))
+    try:
+        log.info(get_drive_status(sys.argv[1]))
+    except:
+        log.exception("Can't read drive status")
+        return 1
 
 
 def monitor():
@@ -22,4 +33,4 @@ def monitor():
 
         status = get_drive_status(d.device_node)
         label = d.properties.get("ID_FS_LABEL", None)
-        print(d.device_node, status, label)
+        log.info(d.device_node, status, label)
