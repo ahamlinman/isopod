d5707b13461f776fd6c9685fb40569cc9cf06950
Alex Hamlin <alex@alexhamlin.co>
2023-12-29 09:11:55 -0800

    Switch isopod_discs to a mapped class

--- isopod/registry.py
+++ isopod/registry.py
@@ -1,22 +1,16 @@
 import logging
-from dataclasses import dataclass
 from enum import Enum, auto
 
-from sqlalchemy import Column, MetaData, String, Table, create_engine, text
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 # Work around SQLAlchemy adding their own handler for echo=True, even though we
 # already have one at the root.
 logging.getLogger("sqlalchemy.engine.Engine").addHandler(logging.NullHandler())
 
-metadata_obj = MetaData()
 
-discs_table = Table(
-    "isopod_discs",
-    metadata_obj,
-    Column("name", String, primary_key=True),
-    Column("status", String),
-)
+class Base(DeclarativeBase):
+    pass
 
 
 class DiscStatus(Enum):
@@ -25,14 +19,15 @@ class DiscStatus(Enum):
     COMPLETE = auto()
 
 
-@dataclass
-class Disc:
-    name: str
-    status: DiscStatus
+class Disc(Base):
+    __tablename__ = "isopod_discs"
+
+    name: Mapped[str] = mapped_column(primary_key=True)
+    status: Mapped[DiscStatus]
 
 
 class Registry:
     def __init__(self, db_path: str):
         # TODO: Remove echo=True after initial debugging.
         self._engine = create_engine(f"sqlite+pysqlite:///{db_path}", echo=True)
-        metadata_obj.create_all(self._engine)
+        Base.metadata.create_all(self._engine)
