65495d50135ccf82aed9fd216135bb38876d0a80
Alex Hamlin <alex@alexhamlin.co>
2023-12-29 07:18:37 -0800

    Move the miscellaneous CLI tools to their own module

--- isopod/__init__.py
+++ isopod/__init__.py
@@ -1,41 +1,7 @@
-import sys
 import logging
-
-from pyudev import Context, Monitor
-
-from .cdrom import get_drive_status
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s %(name)s %(levelname)s: %(message)s",
     datefmt="%F %T",
 )
-
-log = logging.getLogger(__name__)
-
-
-def status():
-    if len(sys.argv) < 2:
-        log.fatal("Need a device to check")
-        return 1
-
-    try:
-        log.info("%s", get_drive_status(sys.argv[1]))
-    except:
-        log.exception("Can't read drive status")
-        return 1
-
-
-def monitor():
-    context = Context()
-    monitor = Monitor.from_netlink(context)
-    try:
-        for d in iter(monitor.poll, None):
-            if not "sr" in (d.driver for d in d.ancestors):
-                continue
-
-            status = get_drive_status(d.device_node)
-            label = d.properties.get("ID_FS_LABEL", None)
-            log.info("%s %s %s", d.device_node, status, label)
-    except KeyboardInterrupt:
-        pass
---
+++ isopod/tool.py
@@ -0,0 +1,35 @@
+import sys
+import logging
+
+from pyudev import Context, Monitor
+
+from .cdrom import get_drive_status
+
+log = logging.getLogger(__name__)
+
+
+def status():
+    if len(sys.argv) < 2:
+        log.fatal("Need a device to check")
+        return 1
+
+    try:
+        log.info("%s", get_drive_status(sys.argv[1]))
+    except:
+        log.exception("Can't read drive status")
+        return 1
+
+
+def monitor():
+    context = Context()
+    monitor = Monitor.from_netlink(context)
+    try:
+        for d in iter(monitor.poll, None):
+            if not "sr" in (d.driver for d in d.ancestors):
+                continue
+
+            status = get_drive_status(d.device_node)
+            label = d.properties.get("ID_FS_LABEL", None)
+            log.info("%s %s %s", d.device_node, status, label)
+    except KeyboardInterrupt:
+        pass
