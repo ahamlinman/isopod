018ecd68605d20224357fd56be022f4a4c3d3ba0
Alex Hamlin <alex@alexhamlin.co>
2023-12-29 09:07:16 -0800

    Use SQLAlchemy's Table to define the schema

--- isopod/registry.py
+++ isopod/registry.py
@@ -2,11 +2,21 @@ import logging
 from dataclasses import dataclass
 from enum import Enum, auto
 
-from sqlalchemy import create_engine, text
+from sqlalchemy import Column, MetaData, String, Table, create_engine, text
+from sqlalchemy.orm import Session
 
 # Work around SQLAlchemy adding their own handler for echo=True, even though we
 # already have one at the root.
 logging.getLogger("sqlalchemy.engine.Engine").addHandler(logging.NullHandler())
+
+metadata_obj = MetaData()
+
+discs_table = Table(
+    "isopod_discs",
+    metadata_obj,
+    Column("name", String, primary_key=True),
+    Column("status", String),
+)
 
 
 class DiscStatus(Enum):
@@ -25,7 +35,4 @@ class Registry:
     def __init__(self, db_path: str):
         # TODO: Remove echo=True after initial debugging.
         self._engine = create_engine(f"sqlite+pysqlite:///{db_path}", echo=True)
-        with self._engine.begin() as conn:
-            conn.execute(
-                text("CREATE TABLE IF NOT EXISTS isopod (hello INTEGER PRIMARY KEY)")
-            )
+        metadata_obj.create_all(self._engine)
