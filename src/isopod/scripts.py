f66614113e8a42b706ca4a9537826c74b9d2b34c
Alex Hamlin <alex@alexhamlin.co>
2023-12-29 21:42:15 -0800

    Turn isopod.cdrom into isopod.udev
    This is now a collection of simple pyudev helpers, only a couple of
    which have anything to do with CD-ROM devices specifically. Let's have
    the names all reflect that.

--- src/isopod/scripts.py
+++ src/isopod/scripts.py
@@ -4,7 +4,7 @@ import os
 import click
 from pyudev import Context, Monitor
 
-import isopod.cdrom
+import isopod.udev
 
 log = logging.getLogger(__name__)
 context_settings = {"help_option_names": ["-h", "--help"]}
@@ -19,10 +19,10 @@ def cli():
 @cli.command()
 def list():
     """List CD-ROM devices on the system."""
-    for dev in isopod.cdrom.get_drives():
+    for dev in isopod.udev.get_cdrom_drives():
         path = dev.device_node
-        if isopod.cdrom.is_drive_loaded(dev):
-            print(f"{path}\t{isopod.cdrom.get_label(dev)}")
+        if isopod.udev.is_cdrom_loaded(dev):
+            print(f"{path}\t{isopod.udev.get_fs_label(dev)}")
         else:
             print(path)
 
@@ -37,7 +37,7 @@ def monitor():
         if not "ID_CDROM" in d.properties:
             continue
 
-        loaded = isopod.cdrom.is_drive_loaded(d)
+        loaded = isopod.udev.is_cdrom_loaded(d)
         label = d.properties.get("ID_FS_LABEL", None)
         log.info("%s\t%s\t%s", d.device_node, loaded, label)
 
@@ -48,11 +48,11 @@ def status(device_path):
     """Print the status of the CD-ROM device at DEVICE_PATH."""
 
     try:
-        dev = isopod.cdrom.get_device(device_path)
+        dev = isopod.udev.get_device(device_path)
         log.info(
             "%s %s",
-            isopod.cdrom.is_drive_loaded(dev),
-            isopod.cdrom.get_label(dev),
+            isopod.udev.is_cdrom_loaded(dev),
+            isopod.udev.get_fs_label(dev),
         )
     except:
         log.exception("Can't read drive status")
--- src/isopod/udev.py
+++ src/isopod/udev.py
@@ -12,19 +12,19 @@ class LocalContext(local):
 CTX = LocalContext()
 
 
-def get_drives() -> Iterable[Device]:
-    return Enumerator(CTX.ctx).match_property("ID_CDROM", "1")
-
-
 def get_device(path: str) -> Device:
     return Devices.from_device_file(CTX.ctx, path)
 
 
-def is_drive_loaded(dev: str | Device) -> bool:
-    dev = get_device(dev) if isinstance(dev, str) else dev
-    return dev.properties.get("ID_CDROM_MEDIA") == "1"
-
-
-def get_label(dev: str | Device) -> Optional[str]:
+def get_fs_label(dev: str | Device) -> Optional[str]:
     dev = get_device(dev) if isinstance(dev, str) else dev
     return dev.properties.get("ID_FS_LABEL")
+
+
+def get_cdrom_drives() -> Iterable[Device]:
+    return Enumerator(CTX.ctx).match_property("ID_CDROM", "1")
+
+
+def is_cdrom_loaded(dev: str | Device) -> bool:
+    dev = get_device(dev) if isinstance(dev, str) else dev
+    return dev.properties.get("ID_CDROM_MEDIA") == "1"
