5a6f3d1b7ee048807adc50358b03e0d6eacc8f4c
Alex Hamlin <alex@alexhamlin.co>
2023-12-29 20:58:41 -0800

    Move all scripts to isopod-scripts subcommands
    This way I don't have to update pyproject.toml for every new script.

--- src/isopod/scripts.py
+++ src/isopod/scripts.py
@@ -3,33 +3,28 @@ import logging
 import click
 from pyudev import Context, Monitor
 
-from isopod.cdrom import get_cdrom_device_paths, get_drive_status, get_fs_label
+from isopod.cdrom import get_cdrom_devices, get_drive_status, get_fs_label
 
 log = logging.getLogger(__name__)
 context_settings = {"help_option_names": ["-h", "--help"]}
 
 
-@click.command(context_settings=context_settings)
+@click.group(context_settings=context_settings)
+def cli():
+    """A harness for testing random parts of isopod."""
+    pass
+
+
+@cli.command()
 def list():
-    """List all CD-ROM devices on the system."""
-    print(get_cdrom_device_paths())
+    """List CD-ROM devices on the system."""
+    for dev in get_cdrom_devices():
+        print(dev.device_node)
 
 
-@click.command(context_settings=context_settings)
-@click.argument("device_path", type=click.Path(exists=True))
-def status(device_path):
-    """Print the status of the CD-ROM drive at DEVICE_PATH as seen by isopod.cdrom."""
-
-    try:
-        log.info("%s %s", get_drive_status(device_path), get_fs_label(device_path))
-    except:
-        log.exception("Can't read drive status")
-        return 1
-
-
-@click.command(context_settings=context_settings)
+@cli.command()
 def monitor():
-    """Watch for udev events on CD-ROM drives and print the status as seen by isopod.cdrom."""
+    """Watch udev events on CD-ROM drives and print status updates."""
 
     context = Context()
     monitor = Monitor.from_netlink(context)
@@ -40,3 +35,15 @@ def monitor():
         status = get_drive_status(d.device_node)
         label = d.properties.get("ID_FS_LABEL", None)
         log.info("%s %s %s", d.device_node, status, label)
+
+
+@cli.command()
+@click.argument("device_path", type=click.Path(exists=True))
+def status(device_path):
+    """Print the status of the CD-ROM device at DEVICE_PATH."""
+
+    try:
+        log.info("%s %s", get_drive_status(device_path), get_fs_label(device_path))
+    except:
+        log.exception("Can't read drive status")
+        return 1
