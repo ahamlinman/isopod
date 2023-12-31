713ed1f2e039d085642a20f0cd522a502098fe52
Alex Hamlin <alex@alexhamlin.co>
2023-12-31 08:42:42 -0800

    Add helper scripts for the isopod-target image
    I'm using this as part of my rsync testing, so I want to make it easy to
    build and run, at least in my own local context.

--- src/isopod/scripts.py
+++ src/isopod/scripts.py
@@ -1,4 +1,9 @@
 import logging
+import shlex
+import signal
+import subprocess
+import sys
+from subprocess import DEVNULL
 
 import click
 from pyudev import Context, Device, Monitor
@@ -37,3 +42,81 @@ def _print_cdrom_info(dev: Device):
     diskseq = isopod.udev.get_diskseq(dev)
     label = isopod.udev.get_fs_label(dev)
     print(f"{dev.device_node}\t{loaded}\t{diskseq}\t{label}")
+
+
+@cli.group()
+def target():
+    """Work with the isopod-target container image."""
+    pass
+
+
+TARGET_IMAGE_NAME = "isopod-target:latest"
+
+
+@target.command(name="build")
+@click.option(
+    "--image", type=str, default=TARGET_IMAGE_NAME, help="The name of the image"
+)
+@click.option(
+    "--build-root",
+    type=click.Path(file_okay=False, dir_okay=True),
+    default="./extras/target",
+    help="The directory containing the Dockerfile",
+)
+def target_build(image, build_root):
+    """Build the isopod-target image in the local Docker daemon."""
+    args = ["docker", "build", f"--tag={image}", build_root]
+    log.info("%s", shlex.join(args))
+    sys.exit(subprocess.run(args).returncode)
+
+
+@target.command(name="run")
+@click.option(
+    "--image", type=str, default=TARGET_IMAGE_NAME, help="The name of the image"
+)
+@click.option(
+    "--address",
+    type=str,
+    default="127.0.0.1",
+    help="The address to bind to on the host",
+)
+@click.option(
+    "--port", type=int, default=11873, help="The port to forward from the host"
+)
+def target_run(image, address, port):
+    """Run the isopod-target image with a persistent volume."""
+
+    args = ["docker", "volume", "inspect", "isopod-target"]
+    proc = subprocess.run(args, stdout=DEVNULL, stderr=DEVNULL)
+    if proc.returncode != 0:
+        args = ["docker", "volume", "create", "isopod-target"]
+        log.info(args)
+        subprocess.run(args, check=True)
+
+        args = [
+            "docker",
+            "run",
+            "--rm",
+            "--volume=isopod-target:/mnt/isopod-target",
+            "busybox:latest",
+            "sh",
+            "-c",
+            "chmod -R 777 /mnt/isopod-target",
+        ]
+        log.info(args)
+        subprocess.run(args, check=True)
+
+    args = [
+        "docker",
+        "run",
+        "--rm",
+        "-it",
+        "--user=nobody:nogroup",
+        "--read-only",
+        "--volume=isopod-target:/mnt/isopod-target",
+        f"--publish={address}:{port}:{873}",
+        image,
+    ]
+    log.info(args)
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    sys.exit(subprocess.run(args).returncode)
