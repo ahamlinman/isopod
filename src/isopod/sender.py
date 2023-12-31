0ac5850f1747c25a6b85b66ca3d13321a0182415
Alex Hamlin <alex@alexhamlin.co>
2023-12-30 18:37:37 -0800

    Implement the basic sending logic
    This accounts for everything except triggering the sender after we've
    successfully ripped a new disc. Not sure if I want to do that by
    watching the store, or by linking the ripper and sender via events.
    
    I should also change the 10 second sleep into an exponential backoff.

--- src/isopod/sender.py
+++ src/isopod/sender.py
@@ -1,5 +1,9 @@
 import logging
+import os
+import subprocess
 import threading
+import time
+from subprocess import DEVNULL
 from threading import Thread
 
 from sqlalchemy import select
@@ -12,19 +16,30 @@ log = logging.getLogger(__name__)
 class Controller(Thread):
     def __init__(self, target_base: str):
         super().__init__(daemon=True)
+
+        if not target_base.endswith("/"):
+            target_base += "/"
+
         self.target_base = target_base
         self.poke = threading.Event()
 
     def run(self):
         self.poke.set()
         while self.poke.wait():
-            self.poke.clear()
-
             if (path := self._get_next_path()) is None:
                 log.info("Waiting for next sendable ISO")
+                self.poke.clear()
                 continue
 
-            log.info("Sending %s", path)
+            args = ["rsync", "--partial", path, f"{self.target_base}/{path}"]
+            log.info("Starting sync: %s", args)
+            proc = subprocess.run(args, stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
+
+            if proc.returncode == 0:
+                self._handle_send_success(path)
+            else:
+                log.error("Sync failed with status %d", proc.returncode)
+                time.sleep(10)
 
     def _get_next_path(self):
         with Session() as session:
@@ -32,3 +47,20 @@ class Controller(Thread):
             disc = session.execute(stmt).scalars().first()
             if disc is not None:
                 return disc.path
+
+    def _handle_send_success(self, path):
+        with Session() as session:
+            stmt = select(Disc).filter_by(path=path)
+            disc = session.execute(stmt).scalar_one()
+            disc.status = DiscStatus.COMPLETE
+            session.commit()
+            log.info("Finished sending %s", path)
+
+            try:
+                os.unlink(path)
+            except FileNotFoundError:
+                pass
+
+            session.delete(disc)
+            session.commit()
+            log.info("Cleaned up %s", path)
