9de02f9cbbabac9727edecadecfed3f23736e51c
Alex Hamlin <alex@alexhamlin.co>
2024-01-01 11:58:46 -0800

    Sketch out most of the new sender
    Have to go somewhere now, no time to actually test this out. But it'll
    probably be good to take a break and come back to this later.

--- src/isopod/newsender.py
+++ src/isopod/newsender.py
@@ -1,19 +1,30 @@
+import logging
+import shlex
 import threading
-from subprocess import Popen
+import time
+from subprocess import DEVNULL, Popen
 from threading import Thread
 from typing import Optional
 
+from sqlalchemy import select
+
+import isopod
 from isopod import db
+
+log = logging.getLogger(__name__)
 
 
 class Controller(Thread):
-    def __init__(self):
+    def __init__(self, target_base: str):
         super().__init__()
+        self.target_base = target_base
+
         self._trigger = threading.Event()
 
         self._canceled = False
 
         self._rsync: Optional[Popen] = None
+        self._current_path: Optional[str] = None
 
     def run(self):
         self._trigger.set()
@@ -25,6 +36,48 @@ class Controller(Thread):
                     self._rsync.terminate()
                     self._rsync.wait()
                 return
+
+            if self._rsync is not None:
+                if (returncode := self._rsync.poll()) is None:
+                    continue
+                self._rsync = None
+                (path, self._current_path) = (self._current_path, None)
+                if returncode == 0:
+                    self._handle_send_success(path)
+                else:
+                    log.info("rsync failed with status %d", returncode)
+                    time.sleep(10)  # TODO: Restore exponential backoff.
+                    self._trigger.set()
+                continue
+
+            if (path := self._get_next_path()) is None:
+                log.info("Waiting for next sendable disc")
+                continue
+
+            args = ["rsync", "--partial", path, f"{self.target_base}/{path}"]
+            self._rsync = Popen(args, stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
+            self._current_path = path
+            log.info("Started: %s", shlex.join(args))
+
+    def _get_next_path(self):
+        with db.Session() as session:
+            stmt = select(db.Disc).filter_by(status=db.DiscStatus.SENDABLE)
+            disc = session.execute(stmt).scalars().first()
+            if disc is not None:
+                return disc.path
+
+    def _handle_send_success(self, path):
+        with db.Session() as session:
+            stmt = select(db.Disc).filter_by(path=path)
+            disc = session.execute(stmt).scalar_one()
+            disc.status = db.DiscStatus.COMPLETE
+            session.commit()
+            log.info("Finished sending %s", path)
+
+            isopod.force_unlink(path)
+            session.delete(disc)
+            session.commit()
+            log.info("Cleaned up %s", path)
 
     def cancel(self):
         self._canceled = True
